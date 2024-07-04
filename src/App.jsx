712e84d216b11eb7def36d88df734f6d562c1159
Abhinav Gupta <gupta380189@gmail.com>
2024-07-04 10:21:56 +0530

    submit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TodoInput from './components/todoinput'
+import Todolist from './components/TodoList'
+
+function App() {
+  const [listTodo,setListTodo]=useState([]);
+  let addList = (inputText)=>{
+    if(inputText!=='')
+      setListTodo([...listTodo,inputText]);
+  }
+  const deleteListItem = (key)=>{
+    let newListTodo = [...listTodo];
+    newListTodo.splice(key,1)
+    setListTodo([...newListTodo])
+  }
+  return (
+    <div className="main-container">
+      <div className="center-container">
+        <TodoInput addList={addList}/>
+        <h2 className="app-heading">TODO</h2>
+        <hr/>
+        {listTodo.map((listItem,i)=>{
+          return (
+            <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
